4294f2c29aa8472c887e50174918a86e99cdbff9
adityasshinde <s.adityashinde11@gmail.com>
2024-03-18 00:46:57 +0530

    all set to deploy

--- internal/handlers/citizens.go
+++ internal/handlers/citizens.go
@@ -2,11 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/adityasshinde/Golang-Backend/internal/models"
 	"github.com/adityasshinde/Golang-Backend/pkg/db"
 	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
 )
 
 func GetCitizens(w http.ResponseWriter, r *http.Request) {
@@ -30,9 +32,23 @@ func GetCitizen(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateCitizen(w http.ResponseWriter, r *http.Request) {
-	var citizen models.Citizen
+	var citizen models.NewCitizen
 	json.NewDecoder(r.Body).Decode(&citizen)
-	err := db.CreateCitizen(citizen)
+	//generate Id for the citizen
+	uuId := uuid.NewV4().String()
+	newCitizen := models.Citizen{
+		Id:          uuId,
+		FirstName:   citizen.FirstName,
+		LastName:    citizen.LastName,
+		DateOfBirth: citizen.DateOfBirth,
+		Gender:      citizen.Address,
+		Address:     citizen.Address,
+		City:        citizen.City,
+		State:       citizen.State,
+		Pincode:     citizen.Pincode,
+	}
+	fmt.Fprint(w, citizen)
+	err := db.CreateCitizen(newCitizen)
 	if err != nil {
 		http.Error(w, "Error creating citizen", http.StatusInternalServerError)
 		return
@@ -56,6 +72,7 @@ func UpdateCitizen(w http.ResponseWriter, r *http.Request) {
 func DeleteCitizen(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	citizenID := params["id"]
+	fmt.Println(" Deleting citizen with id: ", citizenID)
 	err := db.DeleteCitizen(citizenID)
 	if err != nil {
 		http.Error(w, "Error deleting citizen", http.StatusInternalServerError)
--- internal/models/citizen.go
+++ internal/models/citizen.go
@@ -1,6 +1,17 @@
 package models
 
 type Citizen struct {
+	Id          string `json:"id,omitempty"`
+	FirstName   string `json:"firstName,omitempty"`
+	LastName    string `json:"lastName,omitempty"`
+	DateOfBirth string `json:"dob,omitempty"`
+	Gender      string `json:"gender,omitempty"`
+	Address     string `json:"address,omitempty"`
+	City        string `json:"city,omitempty"`
+	State       string `json:"state,omitempty"`
+	Pincode     string `json:"pincode,omitempty"`
+}
+type NewCitizen struct {
 	FirstName   string `json:"firstName,omitempty"`
 	LastName    string `json:"lastName,omitempty"`
 	DateOfBirth string `json:"dob,omitempty"`
